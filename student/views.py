e4582eda1082943c79ea24850cba6e92de80f0c5
Khushigohel <gohelkhushi2004@gmailcom>
2025-12-14 10:51:44 +0530

    create crud operation

---
+++ student/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render,redirect
+from .models import Student
+from django.contrib import messages
+# Create your views here.
+def get_student(request):
+    students=Student.objects.all()
+    return render(request,'student_list.html',{'data':students})
+
+def add_student(request):
+    if request.method== "POST":
+        name=request.POST['name']
+        email=request.POST['email']
+        age=request.POST['age']
+
+        #validation
+        '''if not name or not email or not age:
+            messages.error(request,"All Filed is required")
+        elif not name.isalpha():
+            messages.error(request, "Name can only contain letters!")
+        elif int(age) < 1 or int(age) > 100:
+            messages.error(request, "Age must be between 1 and 100")'''
+
+        Student.objects.create(sname=name,semail=email,sage=age)
+        return redirect('student_list')
+    return render(request,'add_student.html')
+
+def edit_student(request,id):
+    student=Student.objects.get(id=id)
+    if request.method=="POST":
+        student.sname=request.POST['name']
+        student.semail=request.POST['email']
+        student.sage=request.POST['age']
+        student.save()
+        return redirect('student_list')
+    return render(request,'edit_student.html',{'data':student})
+
+def delete_student(request,id):
+    student=Student.objects.get(id=id)
+    student.delete()
+    return redirect('student_list')
+
